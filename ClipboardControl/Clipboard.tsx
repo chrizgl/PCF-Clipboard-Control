6212358007182343e3838b6a34a79c66bda7723d
Christian Gierl <c.gierl@outlook.com>
2023-08-27 15:18:11 +0200

    bugs fixed and design changed to match new look

--- ClipboardControl/Clipboard.tsx
+++ ClipboardControl/Clipboard.tsx
@@ -1,25 +1,40 @@
 import * as React from 'react';
 import * as copy from 'copy-to-clipboard';
-import { Icon } from '@fluentui/react';
+import { Checkmark32Regular, Checkmark32Filled, Copy32Regular, CheckboxChecked24Regular } from '@fluentui/react-icons';
 import {
     makeStyles,
     mergeClasses,
-    tokens,
-    shorthands,
     Button,
     FluentProvider,
     webLightTheme,
     Input,
     InputProps,
     useId,
+    shorthands,
+    tokens,
+    ColorPaletteTokens,
+    Overflow,
 } from '@fluentui/react-components';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 export interface IClipboardProps {
     input: string;
     isPassword: boolean;
     isDisabled: boolean;
-    onInputChange: (input: string) => void;
+    onInputChange: (text: string) => void;
+}
+
+export interface IClipboardState {
+    currentText: string;
+    overlayHidden: boolean;
+    iconBackground: string;
+}
+
+interface IClipboardClassObject {
+    clipboardIcon: string;
+    textbox: string;
+    iconWrapper: string;
+    textboxOverlay: string;
 }
 
 const useStyles = makeStyles({
@@ -72,31 +87,59 @@ const useStyles = makeStyles({
         paddingTop: '0px',
         paddingBottom: '0px',
     },
+    overflow: {
+        color: 'forestgreen',
+        scale: 1.5,
+    },
+    icon: {
+        scale: 1.3,
+    },
+    input: {
+        ...shorthands.border('0px', 'solid', tokens.colorNeutralStroke1),
+        backgroundColor: '#f5f5f5',
+    },
 });
 
-const ClipboardControlApp = (props: IClipboardProps): React.JSX.Element => {
+const ClipboardControlApp = (props: IClipboardProps): JSX.Element => {
     const classes = useStyles();
     const stackClasses = mergeClasses(classes.stack, classes.stackHorizontal);
-    const iconOnClick = () => {
-        copy(inputValue);
-    };
+    const overflowClass = mergeClasses(classes.overflow, classes.stackitem);
+    const inputClass = mergeClasses(classes.input, classes.stackitem);
+    const iconClass = mergeClasses(classes.icon, classes.stackitem);
+
     const id = useId();
     const [inputValue, setInputValue] = useState(props.input);
+    const [state, setState] = useState<IClipboardState>({
+        currentText: props.input,
+        overlayHidden: true,
+        iconBackground: 'transparent',
+    });
     const onInputChange: InputProps['onChange'] = (_, data) => {
-        setInputValue(data.value);
-        props.onInputChange(data.value);
+        setInputValue(data?.value ?? '');
+        props.onInputChange(data?.value ?? '');
     };
-    useEffect(() => {
-        if (inputValue !== props.input) {
-            setInputValue(props.input);
+
+    const iconOnClick = () => {
+        copy(inputValue);
+        setState((state) => ({ ...state, overlayHidden: false, iconBackground: 'lightgreen' }));
+        setTimeout(() => {
+            setState((state) => ({ ...state, overlayHidden: true, iconBackground: 'transparent' }));
+        }, 1000);
+    };
+
+    const showIcon = () => {
+        if (state.overlayHidden) {
+            return <Copy32Regular className={iconClass}></Copy32Regular>;
+        } else {
+            return <Checkmark32Filled className={overflowClass}></Checkmark32Filled>;
         }
-    }, [props.input, inputValue]);
+    };
 
     return (
         <FluentProvider theme={webLightTheme}>
             <div className={stackClasses}>
-                <Input id={id} className={classes.stackitem} value={inputValue} onChange={onInputChange} />
-                <Button className={classes.stackitem} icon={<Icon iconName='Copy' onClick={iconOnClick} />}></Button>
+                <Input id={id} readOnly={props.isDisabled} className={inputClass} value={inputValue} onChange={onInputChange} />
+                <Button className={classes.stackitem} icon={showIcon()} onClick={iconOnClick}></Button>
             </div>
         </FluentProvider>
     );
